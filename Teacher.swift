c2071a5b95381ed2058420da228449228cc1581c
huangwu1111 <1243930473@qq.com>
2023-08-08 13:11:41 +0800

    teacher
    添加teacher

---
+++ Teacher.swift
@@ -0,0 +1,20 @@
+//
+//  Teacher.swift
+//  TestAction
+//
+//  Created by whqpMac007 on 2023/8/8.
+//
+
+import Foundation
+
+
+public class Teacher {
+    public init() {
+        
+    }
+    
+    public func teachStudent() {
+        
+        print("teachStudent")
+    }
+}
